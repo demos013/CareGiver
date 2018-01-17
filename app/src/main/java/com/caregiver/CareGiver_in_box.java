110e2b594f4e536f3db3c2e14daf075fb800f9f3
pndpndpndpnd <pndpndpndpnd@users.noreply.github.com>
2018-01-18 00:08:18 +0700

    you told me to push i push

---
+++ app/src/main/java/com/caregiver/CareGiver_in_box.java
@@ -0,0 +1,13 @@
+package com.caregiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CareGiver_in_box extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care_giver_in_box);
+    }
+}
---
+++ app/src/main/java/com/caregiver/CustomListview/elderly_adapter.java
@@ -0,0 +1,43 @@
+package com.caregiver.CustomListview;
+
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by pndpndpndpnd on 1/18/2018.
+ */
+
+public class elderly_adapter extends BaseAdapter {
+
+    public elderly_adapter() {
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence[] getAutofillOptions() {
+        return new CharSequence[0];
+    }
+}
